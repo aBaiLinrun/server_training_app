dba0f1ba4e4828066f1f55f1d2c941250a0e8eef
Bai Linrun <bai.linrun@access-company.com>
2018-05-08 10:19:31 +0900

    Add view recent room history

--- lib/myapp.ex
+++ lib/myapp.ex
@@ -15,4 +15,35 @@ defmodule Myapp do
   def hello do
     :world
   end
+
+  alias Hipchat.ApiClient
+  alias Hipchat.Httpc.Response
+  alias Hipchat.V2.Rooms
+
+  def main([room_id, msg]) do
+    IO.puts("\nHello Cosmos, from elixir MyApp!\n")
+    '''
+    client = ApiClient.new(load_chat_token())
+    {
+      {:ok, %Response{status: 201}} = Rooms.send_message(client, room_id, %{message: msg})
+    }
+    '''
+    client = ApiClient.new(load_view_token())
+    {
+      {:ok, %Response{status: 201}} = Rooms.get_recent_room_history(client, room_id, "reverse")
+    }
+  end
+
+  defp load_chat_token() do
+    Path.expand("~/.config/hipchat_cli/chat_token")
+    |> File.read!()
+    |> String.trim()
+  end
+
+  defp load_view_token() do
+    Path.expand("~/.config/hipchat_cli/view_token")
+    |> File.read!()
+    |> String.trim()
+  end
+
 end
